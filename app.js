511885b8eba70c0483ec5ebaf34414f58d001be8
varun sachdeva <sachdevavarun7@gmai.com>
2020-09-02 23:03:22 +0530

    add cookies and roles collection

---
+++ app.js
@@ -0,0 +1,134 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: 'littlesecret.',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const roleSchema = new mongoose.Schema({
+  email: String,
+  role: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+const Role = new mongoose.model("Role", roleSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  req.login(user, function(err){
+    if(err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+  });
+}});
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets")
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/register", function(req, res){
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      const newRole = new Role({
+        email: req.body.username,
+        role: req.body.role
+      })
+      newRole.save(function(err){
+        if(err){
+          console.log(err);
+        } else {
+          passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets");
+          });
+        }
+      })
+    }
+  });
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/compose", function(req, res){
+  if (req.isAuthenticated()){
+    Role.findOne({email: req.user.username}, function(err, foundRole){
+      if(foundRole.role === "admin"){
+        console.log("welcome admin");
+        res.redirect("/secrets");
+      } else {
+        console.log("fuck off user");
+        res.redirect("/");
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.listen(3000, function(){
+  console.log("server is a go");
+});
