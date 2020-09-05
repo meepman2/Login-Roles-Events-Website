14784344e005c994e8df98a94feb02708c8f1a6a
varun sachdeva <sachdevavarun7@gmai.com>
2020-09-05 10:58:18 +0530

    add env variables

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB, {useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
